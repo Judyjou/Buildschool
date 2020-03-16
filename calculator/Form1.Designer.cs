6262b8e5504bbbe7ca6f10597e8930137efb5cf6
aquariusjou@gmail.com <aquariusjou@gmail.com>
2020-03-16 14:35:43 +0800

    botton

--- calculator/Form1.Designer.cs
+++ calculator/Form1.Designer.cs
@@ -28,13 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Btn_sub = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Btn_sub
+            // 
+            this.Btn_sub.Location = new System.Drawing.Point(298, 280);
+            this.Btn_sub.Name = "Btn_sub";
+            this.Btn_sub.Size = new System.Drawing.Size(110, 62);
+            this.Btn_sub.TabIndex = 0;
+            this.Btn_sub.Text = "Enter";
+            this.Btn_sub.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Btn_sub);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button Btn_sub;
     }
 }
 
