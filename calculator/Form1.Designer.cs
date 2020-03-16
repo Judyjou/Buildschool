597666852c52aee1a69e4859b5fa2269a3e7a42e
YuChen <xup6m45p1880@gmail.com>
2020-03-16 14:38:04 +0800

    1

--- calculator/Form1.Designer.cs
+++ calculator/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.Btn_sub = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // Btn_sub
@@ -40,11 +41,21 @@
             this.Btn_sub.Text = "Enter";
             this.Btn_sub.UseVisualStyleBackColor = true;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(139, 131);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 89);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.Btn_sub);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -55,6 +66,7 @@
         #endregion
 
         private System.Windows.Forms.Button Btn_sub;
+        private System.Windows.Forms.Button button1;
     }
 }
 
